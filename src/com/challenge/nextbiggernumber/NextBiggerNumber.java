afe0ba662763b39956f1a038db1ba606d1c173c9
RomanFedorenko <frank.lamo@ukr.net>
2024-10-07 01:37:26 +0300

    Added 'NextBiggerNumber' kata solution with updated tests

--- src/com/challenge/nextbiggernumber/NextBiggerNumber.java
+++ src/com/challenge/nextbiggernumber/NextBiggerNumber.java
@@ -1,6 +1,7 @@
 package com.challenge.nextbiggernumber;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,35 +11,50 @@ import java.util.stream.Collectors;
  * 12 ==> 21
  * 513 ==> 531
  * 2017 ==> 2071
+ * 210 666 0501 ==> 210 666 0510
+ * 598 848 484 598 53 ==> 598 848 484 835 59
+ * 819 967 532 ==> 819 972 356
+ * 737 790 073 ==> 737 790 307
  * If the digits can't be rearranged to form a bigger number, return -1
  * <p>
  * 9 ==> -1
  * 111 ==> -1
  * 531 ==> -1
- * 210 666 0501 ==> 210 666 0510
- * 5988 4848 45 9853 ==> 5988 4848 48 3559
- * 819 967 532 ==> 819 972 356
  */
 
 public class NextBiggerNumber {
     public static long nextBiggerNumber(long n) {
-        String str = String.valueOf(n);
-        if (str.length() == 1) return -1;
-        List<Integer> ints = str.chars().map(Character::getNumericValue)
+        List<Integer> digits = String.valueOf(n).chars()
+                .map(Character::getNumericValue)
                 .boxed()
                 .collect(Collectors.toList());
-        int swapIndex = -1;
 
-        for (int i = 1; i < ints.size(); i++) {
-            if (ints.get(i) > ints.get(i - 1)) {
-                swapIndex = i - 1;
+        int firstSwapIndex = -1;
+        for (int i = 1; i < digits.size(); i++) {
+            if (digits.get(i) > digits.get(i - 1)) {
+                firstSwapIndex = i - 1;
+            }
+        }
+        if (firstSwapIndex == -1) return -1;
+
+        int firstSwapElement = digits.get(firstSwapIndex);
+        Integer secondSwapElement = null;
+        Integer secondSwapIndex = -1;
+        for (int i = firstSwapIndex + 1; i < digits.size(); i++) {
+            if (digits.get(i) > firstSwapElement) {
+                if (secondSwapElement == null || digits.get(i) < secondSwapElement) {
+                    secondSwapElement = digits.get(i);
+                    secondSwapIndex = i;
+                }
+
             }
         }
 
-        if (swapIndex == -1) return -1;
-        Collections.swap(ints, swapIndex, ++swapIndex);
-        if (ints.get(ints.size() - 1) == 0) Collections.swap(ints, ints.size() - 1, ints.size() - 2);
-        String result = ints.stream().map(String::valueOf).collect(Collectors.joining());
-        return Long.parseLong(result);
+        Collections.swap(digits, firstSwapIndex, secondSwapIndex);
+        if (secondSwapIndex < digits.size()) {
+            digits.subList(firstSwapIndex + 1, digits.size()).sort(Comparator.naturalOrder());
+        }
+
+        return Long.parseLong(digits.stream().map(String::valueOf).collect(Collectors.joining()));
     }
 }
--- src/com/challenge/test/NextBiggerNumberTest.java
+++ src/com/challenge/test/NextBiggerNumberTest.java
@@ -8,11 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class NextBiggerNumberTest {
     @Test
     public void basicTests() {
+        assertEquals(737790307L, NextBiggerNumber.nextBiggerNumber(737790073L));
         assertEquals(21, NextBiggerNumber.nextBiggerNumber(12));
         assertEquals(531, NextBiggerNumber.nextBiggerNumber(513));
         assertEquals(2071, NextBiggerNumber.nextBiggerNumber(2017));
         assertEquals(441, NextBiggerNumber.nextBiggerNumber(414));
         assertEquals(414, NextBiggerNumber.nextBiggerNumber(144));
         assertEquals(19009, NextBiggerNumber.nextBiggerNumber(10990));
+        assertEquals(59884848483559L, NextBiggerNumber.nextBiggerNumber(59884848459853L));
+        assertEquals(819972356L, NextBiggerNumber.nextBiggerNumber(819967532L));
+        assertEquals(2106660510L, NextBiggerNumber.nextBiggerNumber(2106660501L));
     }
 }
