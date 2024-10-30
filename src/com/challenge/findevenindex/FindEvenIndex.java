da44c0903d98a31cfffe0c09a24412ce7ee81bce
RomanFedorenko <frank.lamo@ukr.net>
2024-10-30 20:34:20 +0200

    Added 'FindEvenIndex' kata solution with tests

---
+++ src/com/challenge/findevenindex/FindEvenIndex.java
@@ -0,0 +1,40 @@
+package com.challenge.findevenindex;
+
+import java.util.Arrays;
+
+/**
+ * You are going to be given an array of integers. Your job is to take that array and find an index N where the sum
+ * of the integers to the left of N is equal to the sum of the integers to the right of N.
+ *
+ * If there is no index that would make this happen, return -1.
+ *
+ * For example:
+ * Let's say you are given the array {1,2,3,4,3,2,1}:
+ * Your function will return the index 3, because at the 3rd position of the array,
+ * the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+ *
+ * Let's look at another one.
+ * You are given the array {1,100,50,-51,1,1}:
+ * Your function will return the index 1, because at the 1st position of the array,
+ * the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+ *
+ * Last one:
+ * You are given the array {20,10,-80,10,10,15,35}
+ * At index 0 the left side is {}
+ * The right side is {10,-80,10,10,15,35}
+ * They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+ * Index 0 is the place where the left side and right side are equal.
+ */
+
+public class FindEvenIndex {
+    public static int findEvenIndex(int[] arr) {
+        int totalSum = Arrays.stream(arr).sum();
+        int leftSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (leftSum == totalSum - leftSum - arr[i]) return i;
+            leftSum += arr[i];
+        }
+        return -1;
+    }
+}
