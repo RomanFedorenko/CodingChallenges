6e618d9722c3c0cfc4cd8ebf551cd99a28e9c308
RomanFedorenko <frank.lamo@ukr.net>
2024-11-04 00:31:25 +0200

    Added 'PickPeaks' kata solution with tests

--- src/com/challenge/pickpeaks/PickPeaks.java
+++ src/com/challenge/pickpeaks/PickPeaks.java
@@ -1,28 +1,31 @@
 package com.challenge.pickpeaks;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PickPeaks {
     public static Map<String, List<Integer>> getPeaks(int[] arr) {
-        System.out.println(Arrays.toString(arr));
-        Map<String, List<Integer>> result = new HashMap<>();
-        result.put("peaks", new ArrayList<>());
-        result.put("pos", new ArrayList<>());
-        int platoPeakValue = Integer.MIN_VALUE;
-        int platoPeakIndex = 0;
+        Map<String, List<Integer>> result = Map.of(
+                "peaks", new ArrayList<>(),
+                "pos", new ArrayList<>()
+        );
+
+        int plateauPeakIndex = 0;
 
         for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i - 1] < arr[i] && arr[i + 1] < arr[i]) {
-                result.get("peaks").add(arr[i]);
-                result.get("pos").add(i);
-            } else if (arr[i - 1] < arr[i] && arr[i + 1] == arr[i]) {
-                platoPeakValue = arr[i];
-                platoPeakIndex = i;
-            }
-            if (arr[i] == platoPeakValue && arr[i + 1] < platoPeakValue) {
-                result.get("peaks").add(platoPeakValue);
-                result.get("pos").add(platoPeakIndex);
-                platoPeakValue = Integer.MIN_VALUE;
+            if (arr[i] > arr[i - 1]) {
+                if (arr[i] > arr[i + 1]) {
+                    result.get("peaks").add(arr[i]);
+                    result.get("pos").add(i);
+                    plateauPeakIndex = 0; // reset any plateau peak if confirmed peak
+                } else if (arr[i] == arr[i + 1]) {
+                    plateauPeakIndex = i; // start plateau
+                }
+            } else if (arr[i] == arr[i - 1] && plateauPeakIndex != 0 && arr[i] > arr[i + 1]) {
+                result.get("peaks").add(arr[plateauPeakIndex]);
+                result.get("pos").add(plateauPeakIndex);
+                plateauPeakIndex = 0; // reset plateau tracking
             }
         }
 
