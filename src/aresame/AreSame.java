a12af71a5fbe5bb225129e4d7970e03f850fb315
RomanFedorenko <frank.lamo@ukr.net>
2024-11-06 22:21:13 +0200

    Added 'AreSame' kata solution with tests

---
+++ src/aresame/AreSame.java
@@ -0,0 +1,14 @@
+package aresame;
+
+import java.util.Arrays;
+
+/**
+ * see description at -> https://www.codewars.com/kata/550498447451fbbd7600041c/java
+ * */
+
+public class AreSame {
+    public static boolean comp(int[] a, int[] b) {
+        if (a==null || b==null || a.length != b.length) return false;
+        return Arrays.equals(Arrays.stream(a).map(x -> x * x).sorted().toArray(), Arrays.stream(b).sorted().toArray());
+    }
+}
